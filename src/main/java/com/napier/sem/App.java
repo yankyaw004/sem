f3c65b23d56cf52485377355b74339ea548a583a
yankyaw004 <40870398@live.napier.ac.uk>
2026-09-16 23:16:21 +0630

    Separate database connect and disconnect methods

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,13 +1,18 @@
 package com.napier.sem;
+
 import java.sql.*;
 
-
 public class App {
-    public static void main(String[] args)
+
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
-            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
@@ -16,27 +21,30 @@ public class App {
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
+
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
+
             try
             {
-                // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root",
+                        "example"
+                );
+
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(
+                        "Failed to connect to database attempt " + i
+                );
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -44,12 +52,17 @@ public class App {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
             {
-                // Close connection
                 con.close();
             }
             catch (Exception e)
@@ -58,4 +71,17 @@ public class App {
             }
         }
     }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
 }
+
