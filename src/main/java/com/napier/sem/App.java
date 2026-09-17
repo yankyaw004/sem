fe04c57116c515ce5c35be57656a8a4d13beecf0
yankyaw004 <40870398@live.napier.ac.uk>
2026-09-17 23:36:52 +0630

    Add salary information printing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -150,6 +150,26 @@ public class App {
         }
     }
 
+    /**
+     * Prints a list of employees.
+     *
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees) {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s",
+                "Emp No", "First Name", "Last Name", "Salary"));
+
+        // Loop over all employees in the list
+        for (Employee emp : employees) {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+
+            System.out.println(emp_string);
+        }
+    }
+
     public static void main(String[] args) {
         // Create new Application
         App a = new App();
@@ -160,8 +180,8 @@ public class App {
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
 
-        // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+        // Print employee salary information
+        a.printSalaries(employees);
 
         // Disconnect from database
         a.disconnect();
